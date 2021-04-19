dd2f374f23c9cc512d4fadcb150d550e822c0c20
KourosBey <atillagmeric@gmail.com>
2021-04-19 21:39:37 +0300

    Dosyalar

--- firebaseTasarim/firebasetasarim/android/app/src/main/kotlin/com/example/firebasetasarim/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.firebasetasarim
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- gazete/android/app/src/main/kotlin/com/example/gazete/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gazete
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
